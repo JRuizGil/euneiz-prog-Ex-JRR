9efadc1a2b4d3eb8800831ff1a8cdcf56826226c
JRuizG <joseramon_ruiz@euneiz.com>
2023-12-18 11:29:16 +0100

    crear lista de numeros y pedir numeros para añadir

--- Ej/Program.cs
+++ Ej/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//create list Numbers(listNumbers) 
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+List<int> listNumbers = new List<int>();
+int Number;
+// ask user to introduce the numbers
+do
+{
+    Console.WriteLine("Ingrese un numero");
+    Number = int.Parse(Console.ReadLine());
+    Console.WriteLine("¿Quieres agregar otro numero? (si/no)");
+
+} while (Console.ReadLine().ToLower() == "si") ;//do while wants to write more numbers.
+
+// show in console the list of written numbers(listNumbers)
+//then order the list(listOrderedNumbers)
+//show in console the ordered list
